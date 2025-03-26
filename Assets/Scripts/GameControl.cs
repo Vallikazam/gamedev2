764af09a7da7d435313b283d2e672e4c892a3300
ValliKaz <alinur35791@gmail.com>
2025-03-26 09:45:50 +0500

    Initial commit

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement; 
+
+public class GameControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject winText; 
+
+    [SerializeField]
+    private Image darkOverlay; 
+
+    [SerializeField]
+    private GameObject restartButton; 
+
+    [SerializeField]
+    private Tiger tiger; 
+    [SerializeField]
+    private Panda panda;   
+    [SerializeField]
+    private Deer deer; 
+    [SerializeField]
+    private Turkey turkey; 
+
+    private bool hasWon = false; 
+
+    void Start()
+    {
+        winText.SetActive(false); 
+        restartButton.SetActive(false); 
+        darkOverlay.color = new Color(darkOverlay.color.r, darkOverlay.color.g, darkOverlay.color.b, 0f); 
+    }
+
+    void Update()
+    {
+        if (!hasWon && Tiger.locked && Panda.locked && Deer.locked && Turkey.locked)
+        {
+            hasWon = true;
+            StartCoroutine(ShowWinScreen()); 
+        }
+    }
+
+    private IEnumerator ShowWinScreen()
+    {
+        float fadeDuration = 1f; 
+        float elapsedTime = 0f;
+        Color startColor = darkOverlay.color; 
+        Color targetColor = new Color(startColor.r, startColor.g, startColor.b, 0.8f); 
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            darkOverlay.color = Color.Lerp(startColor, targetColor, elapsedTime / fadeDuration); 
+            yield return null;
+        }
+
+        winText.SetActive(true);
+        restartButton.SetActive(true);
+    }
+    public void RestartGame()
+{
+    if (tiger != null) Tiger.locked = false;
+    if (panda != null) Panda.locked = false;
+    if (deer != null) Deer.locked = false;
+    if (turkey != null) Turkey.locked = false;
+
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+}
+}
---
+++ Assets/Scripts/Tiger.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiger : MonoBehaviour
+{
+    [SerializeField]
+    private Transform tigerPlace;
+    private Vector2 initialPosition;
+    private float deltaX, deltaY;
+    public static bool locked;
+    private bool isDragging = false;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        // Touch Screen
+        if (Input.touchCount > 0 && !locked)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+                        deltaX = touchPos.x - transform.position.x;
+                        deltaY = touchPos.y - transform.position.y;
+                        isDragging = true;
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    if (isDragging)
+                        transform.position = new Vector2(touchPos.x - deltaX, touchPos.y - deltaY);
+                    break;
+
+                case TouchPhase.Ended:
+                    isDragging = false;
+                    CheckPosition();
+                    break;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!locked)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(mousePos))
+            {
+                deltaX = mousePos.x - transform.position.x;
+                deltaY = mousePos.y - transform.position.y;
+                isDragging = true;
+            }
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (isDragging && !locked)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(mousePos.x - deltaX, mousePos.y - deltaY);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (isDragging)
+        {
+            isDragging = false;
+            CheckPosition();
+        }
+    }
+
+    private void CheckPosition()
+    {
+        if (Mathf.Abs(transform.position.x - tigerPlace.position.x) <= 0.5f &&
+            Mathf.Abs(transform.position.y - tigerPlace.position.y) <= 0.5f)
+        {
+            transform.position = new Vector2(tigerPlace.position.x, tigerPlace.position.y);
+            locked = true;
+        }
+        else
+        {
+            transform.position = initialPosition;
+        }
+    }
+}
